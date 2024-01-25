9d5191bbda493d0dfaff08fe22f3cc74e350986c
alex bgx <a.bourgeaux@gmail.com>
2024-01-25 10:42:46 +0100

    on my way 2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,17 +14,17 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
+    if @list.save
       redirect_to list_path(@list)
-    # else
-    #   render :new, status: :unprocessable_entity
-    # end
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-  
+
 end
 
 private
 
 def list_params
-  params.require(:list).permit(:name)
+  params.require(:list).permit(:name, :photo)
 end
